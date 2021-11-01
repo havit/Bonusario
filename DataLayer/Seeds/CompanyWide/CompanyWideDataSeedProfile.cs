954cb498aad98dae5f063d3b8eba3e2d4e48e068
Robert Haken <haken@havit.cz>
2021-11-01 23:45:51 +0100

    DataSeed profiles

---
+++ DataLayer/Seeds/CompanyWide/CompanyWideDataSeedProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Data.Patterns.DataSeeds.Profiles;
+
+namespace Havit.Bonusario.DataLayer.Seeds.CompanyWide
+{
+	public class CompanyWideDataSeedProfile : DataSeedProfile
+	{
+	}
+}
--- DataLayer/Seeds/Core/EmployeeSeed.cs
+++ DataLayer/Seeds/Core/EmployeeSeed.cs
@@ -22,31 +22,12 @@ namespace Havit.Bonusario.DataLayer.Seeds.Core
 		{
 			var employees = new Employee[]
 			{
-				new Employee() { Name = "Jan Babušík", Email = "babusik@havit.cz" },
 				new Employee() { Name = "Dominik Crha", Email = "crha@havit.cz" },
-				new Employee() { Name = "Gabriela Čílová", Email = "cilova@havit.cz" },
-				//new Employee() { Name = "Jiří Činčura", Email = "cincura@havit.cz" },
-				new Employee() { Name = "Renáta Drozdová", Email = "drozdova@havit.cz" },
-				new Employee() { Name = "Jiří Gregor", Email = "gregor@havit.cz" },
-				new Employee() { Name = "Alexandr Hájek", Email = "hajek@havit.cz" },
 				new Employee() { Name = "Robert Haken", Email = "haken@havit.cz" },
-				new Employee() { Name = "Zbyšek Hlinka", Email = "hlinka@havit.cz" },
-				//new Employee() { Name = "Mikuláš Hoblík", Email = "hoblik@havit.cz" },
 				new Employee() { Name = "Daniel Hrubý", Email = "hruby@havit.cz" },
 				new Employee() { Name = "Jiří Kanda", Email = "kanda@havit.cz" },
-				new Employee() { Name = "Miloslav Kašpárek", Email = "kasparek@havit.cz" },
 				new Employee() { Name = "Martin Kochman", Email = "kochman@havit.cz" },
-				new Employee() { Name = "Viliam Kortiš", Email = "kortis@havit.cz" },
-				new Employee() { Name = "Pavel Kříž", Email = "kriz@havit.cz" },
-				new Employee() { Name = "Miroslav Louma", Email = "louma@havit.cz" },
-				new Employee() { Name = "Michael Melena", Email = "melena@havit.cz" },
-				new Employee() { Name = "Jakub Nábělek", Email = "nabelek@havit.cz" },
-				new Employee() { Name = "Pavel Růžička", Email = "ruzicka@havit.cz" },
-				new Employee() { Name = "Petr Svoboda", Email = "svoboda@havit.cz" },
-				new Employee() { Name = "Jiří Šáda", Email = "sada@havit.cz" },
-				new Employee() { Name = "Ondřej Václavek", Email = "vaclavek@havit.cz" },
 				new Employee() { Name = "Igor Vít", Email = "vit@havit.cz" },
-				new Employee() { Name = "Tomáš Wagner", Email = "wagner@havit.cz" },
 			};
 
 			foreach (var employee in employees)
