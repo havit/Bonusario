bff4cbe73a3f4b0ca490b68f0c18837447877356
Dominik Crha <crha@havit.cz>
2021-11-03 12:33:35 +0100

    Build fix

--- Web.Client/Components/RemainingPoints.razor.cs
+++ Web.Client/Components/RemainingPoints.razor.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Components;
 
 namespace Havit.Bonusario.Web.Client.Components
 {
-    public partial class  RemainingPoints
-    {
-        [Parameter] public int Points { get; set; }
-    }
+	public partial class RemainingPoints
+	{
+		[Parameter] public int Points { get; set; }
+	}
 }
