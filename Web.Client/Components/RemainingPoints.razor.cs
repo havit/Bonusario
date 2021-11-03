826e3033e6eea447231aa5e4fff89bd589bb1bcf
Dominik Crha <crha@havit.cz>
2021-11-03 12:31:03 +0100

    Remaining points komponenta, EntriesTable cleanup

---
+++ Web.Client/Components/RemainingPoints.razor.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.Bonusario.Web.Client.Components
+{
+    public partial class  RemainingPoints
+    {
+        [Parameter] public int Points { get; set; }
+    }
+}
