fc9b21feecf33db2ec26523e8869002ff9b68d0a
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-10-05 18:14:45 +0200

    [UX] EntryCard - separate editing from viewing #24 (WIP)

---
+++ Web.Client/Components/ReadonlyEntryCard.razor.cs
@@ -0,0 +1,16 @@
+﻿using Havit.Bonusario.Web.Client.DataStores;
+
+namespace Havit.Bonusario.Web.Client.Components;
+
+public partial class ReadonlyEntryCard
+{
+	[Parameter] public EntryDto Entry { get; set; }
+	[Parameter] public bool ShowAuthor { get; set; }
+
+	[Inject] protected IEmployeesDataStore EmployeesDataStore { get; set; }
+
+	protected override async Task OnInitializedAsync()
+	{
+		await EmployeesDataStore.EnsureDataAsync();
+	}
+}
