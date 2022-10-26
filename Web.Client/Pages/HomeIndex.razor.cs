13017c052e9cfc4d135d5180a539453096fafb79
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-10-26 10:16:11 +0200

    HomeIndex - presentation of period active for submissions

--- Web.Client/Pages/HomeIndex.razor.cs
+++ Web.Client/Pages/HomeIndex.razor.cs
@@ -7,10 +7,12 @@ public partial class HomeIndex
 	[Inject] protected IPeriodsDataStore PeriodsDataStore { get; set; }
 
 	private int? periodId;
+	private PeriodDto period;
 
 	protected override async Task OnInitializedAsync()
 	{
 		await PeriodsDataStore.EnsureDataAsync();
-		periodId ??= PeriodsDataStore.GetActiveForSubmission().OrderBy(p => p.EndDate).FirstOrDefault()?.PeriodId;
+		period ??= PeriodsDataStore.GetActiveForSubmission().OrderBy(p => p.EndDate).FirstOrDefault();
+		periodId = period?.PeriodId;
 	}
 }
